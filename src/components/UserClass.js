5d48253eb0be911ebbd606eec3bc9b4cb3073534
Mahadev Chidanand Athani <athanimahadev348@gmail.com>
2024-11-07 00:15:27 +0530

    more details about lifecycle

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -3,34 +3,63 @@ increase counts on button click. */
 
 import React from "react";
 
-class UserClass extends React.Component{
+class UserClass extends React.Component {
 
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={
-      userInfo:{
-        name:"maha",
-        location:"default",
+    this.state = {
+      userInfo: {
+        name: "maha",
+        location: "default",
+        avatar_url: "hguijgjkg.////",
       }
     }
-   
+
   }
-  async componentDidMount(){
+  async componentDidMount() {
     const data = await fetch("https://api.github.com/users/mahadevathani0017");
-    const json= await data.json();
+    const json = await data.json();
     this.setState({
-      userInfo:json,
+      userInfo: json,
     })
     console.log(json);
   }
-    render(){
-      return (
-          <div className="user-card">
-            <h2>Name:{this.state.userinfo.name}</h2>
-            <h3>Location:{location}</h3>
-            <h3>Contact:896797998979</h3>
-          </div>
-        );
-    }
+  componentDidUpdate(){
+    console.log("Update did mount");
+  }
+
+  componentWillUnmount(){
+    console.log("Will unmount called");
+  }
+  render() {
+    const { name, location, avatar_url } = this.state.userInfo;
+    return (
+      <div className="user-card">
+        <h2>Name:{name}</h2>
+        <img src={avatar_url} />
+        <h3>Location:{location}</h3>
+        <h3>Contact:896797998979</h3>
+      </div>
+    );
+  }
 }
 export default UserClass;
+
+/**
+ * 
+ * Construtor (dummy)
+ * Render (dummy)
+ *   <HTML Dummy>
+ * Component Did Mount
+ *   <API call>
+ *   <this.setState>
+ *
+ * -----UPDATE
+ * 
+ *     render(API Data)
+ *     <HTML data(api Data)
+ * componentDid Update
+ * 
+ *
+ * 
+ */

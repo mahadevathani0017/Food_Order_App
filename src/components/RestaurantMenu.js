1ea5a44e5c9657a511883471919031dd92e059df
Mahadev Chidanand Athani <athanimahadev348@gmail.com>
2024-11-30 23:35:50 +0530

    update and lightweight the code

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -8,16 +8,16 @@
  * If there are no items available, it displays a message "No items available".
  */
 
-
 import React, { useEffect, useState } from "react";
 import Shimmer from "./Shimmer";
 import { useParams } from "react-router-dom";
 import { MENU_API_URL } from "../utils/constants";
-
+import useRestaurantMenu from "../utils/useRestaurantMenu";
 const RestaurantMenu = () => {
-  const [resInfo, setResInfo] = useState(null);
+  
 
   const { resId } = useParams();
+  const resInfo=useRestaurantMenu(resId);
 
   useEffect(() => {
     fetchMenu();
@@ -27,19 +27,18 @@ const RestaurantMenu = () => {
     try {
       const data = await fetch(`${MENU_API_URL}${resId}`);
       const json = await data.json();
-      console.log("Fetched Data:", json); // Log the fetched data for debugging
+      console.log("Fetched Data:", json); 
       setResInfo(json.data);
     } catch (error) {
       console.error("Error fetching menu:", error);
     }
   };
 
-  // Extract items and restaurant info
+
   const itemCards =
     resInfo?.cards?.[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards?.[1]?.card
       ?.card?.itemCards || [];
 
-  // Log itemCards to check if it's correctly assigned
   console.log("Item Cards:", itemCards);
 
   const name = resInfo?.cards?.[2]?.card?.card?.info?.name;

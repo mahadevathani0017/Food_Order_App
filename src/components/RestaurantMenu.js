c54423191e5ac2aee133b4747a3347c58b29c02d
Mahadev Chidanand Athani <athanimahadev348@gmail.com>
2024-12-08 22:31:55 +0530

    setup tailwind CSS to styling our website

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,5 +1,5 @@
 import { useParams } from "react-router-dom";
-import { useState, useEffect } from "react";
+
 import Shimmer from "./Shimmer";
 
 import useRestaurantMenu from "../utils/useRestaurantMenu";
